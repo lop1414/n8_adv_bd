5242f64c41d080a1f874cd9c98ab44f27b955e03
LinYouXin <310467466@qq.com>
2021-08-10 17:22:23 +0800

    点击数据 兼容字符串NULL

--- app/Services/AdvClickService.php
+++ app/Services/AdvClickService.php
@@ -32,6 +32,12 @@ class AdvClickService extends ClickService
         ]);
         Functions::hasEnum(AdvClickSourceEnum::class, $data['click_source']);
 
+        if($data['imei'] == 'NULL') $data['imei'] = null;
+        if($data['idfa'] == 'NULL') $data['idfa'] = null;
+        if($data['muid'] == 'NULL') $data['muid'] = null;
+        if($data['android_id'] == 'NULL') $data['android_id'] = null;
+        if($data['oaid'] == 'NULL') $data['oaid'] = null;
+
         $muid = '';
         if(!empty($data['imei'])){
             $muid = trim($data['imei']);
